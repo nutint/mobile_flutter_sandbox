40a9b9bc1e7145b364f6af53bf4e3323cf491c1b
Nat-macbook <nattha.int@gmail.com>
2023-01-28 15:27:31 +0700

    Add building function for different case of tennis score

--- lib/sandbox/tennis.dart
+++ lib/sandbox/tennis.dart
@@ -1,32 +1,93 @@
 import 'package:equatable/equatable.dart';
 
 enum Score {
-  Love,
-  Fifteen,
-  Thirty,
+  love,
+  fifteen,
+  thirty,
+  forty,
 }
 
 class TennisCalculator {
-  static InitialTennisGame build({
+  static TennisGame build({
     Score? playerAScore,
     Score? playerBScore,
   }) {
-    return InitialTennisGame(
-      playerAScore: playerAScore ?? Score.Love,
-      playerBScore: playerBScore ?? Score.Love,
-    );
+    var calculatedPlayerAScore = playerAScore ?? Score.love;
+    var calculatedPlayerBScore = playerBScore ?? Score.love;
+
+    if(calculatedPlayerAScore == Score.love && calculatedPlayerBScore == Score.love) {
+      return InitialTennisGame(
+        playerAScore: calculatedPlayerAScore,
+        playerBScore: calculatedPlayerBScore,
+      );
+    } else if(calculatedPlayerAScore == Score.forty && calculatedPlayerBScore == Score.forty) {
+      return const DeuceTennisGame();
+    } else {
+      return PlayingTennisGame(
+        playerAScore: calculatedPlayerAScore,
+        playerBScore: calculatedPlayerBScore,
+      );
+    }
   }
+
+  static PlayerAAdvantageTennisGame buildPlayerAAdvantage() =>
+      const PlayerAAdvantageTennisGame();
+
+  static PlayerBAdvantageTennisGame buildPlayerBAdvantage() =>
+      const PlayerBAdvantageTennisGame();
+
+  static PlayerAWonTennisGame buildPlayerAWon() => const PlayerAWonTennisGame();
+  static PlayerBWonTennisGame buildPlayerBWon() => const PlayerBWonTennisGame();
 }
 
-class InitialTennisGame extends Equatable{
+class TennisGame extends Equatable {
   final Score playerAScore;
   final Score playerBScore;
 
-  const InitialTennisGame({
+  const TennisGame({
     required this.playerAScore,
     required this.playerBScore,
   });
 
   @override
   List<Object?> get props => [playerAScore, playerBScore];
+}
+
+class InitialTennisGame extends TennisGame {
+  const InitialTennisGame({
+    required playerAScore,
+    required playerBScore,
+  }) : super(playerAScore: playerAScore, playerBScore: playerBScore);
+}
+
+class PlayingTennisGame extends TennisGame {
+  const PlayingTennisGame({
+    required playerAScore,
+    required playerBScore,
+  }) : super(playerAScore: playerAScore, playerBScore: playerBScore);
+}
+
+class DeuceTennisGame extends TennisGame {
+  const DeuceTennisGame()
+    : super(playerAScore: Score.forty, playerBScore: Score.forty);
+}
+
+class PlayerAAdvantageTennisGame extends TennisGame {
+  const PlayerAAdvantageTennisGame()
+    : super(playerAScore: Score.love, playerBScore: Score.love);
+}
+
+class PlayerBAdvantageTennisGame extends TennisGame {
+  const PlayerBAdvantageTennisGame()
+    : super(playerAScore: Score.love, playerBScore: Score.love);
+}
+
+class PlayerAWonTennisGame extends TennisGame {
+  const PlayerAWonTennisGame()
+    : super(playerAScore: Score.love, playerBScore: Score.love);
+}
+
+class PlayerBWonTennisGame extends TennisGame {
+  const PlayerBWonTennisGame()
+    : super(playerAScore: Score.love, playerBScore: Score.love);
 }
--- test/sandbox/tennis_test.dart
+++ test/sandbox/tennis_test.dart
@@ -1,35 +1,74 @@
 import 'package:test/test.dart';
 import 'package:my_app/sandbox/tennis.dart';
 
-void main()
-{
-  group('tennisCalculator', () {
-    test('should be able to init with score', () {
-      var actual = TennisCalculator.build();
+void main() {
+  group('TennisScoreCalculator', () {
+    group('build', () {
+      test('should be able to init with score', () {
+        var actual = TennisCalculator.build();
 
-      expect(actual, const InitialTennisGame(
-        playerAScore: Score.Love,
-        playerBScore: Score.Love,
-      ));
-    });
+        expect(actual, const InitialTennisGame(
+          playerAScore: Score.love,
+          playerBScore: Score.love,
+        ));
+      });
 
-    test('should be able to init with score', () {
-      var actual = TennisCalculator.build(
-        playerAScore: Score.Fifteen,
-        playerBScore: Score.Thirty,
-      );
+      test('should be able to init with initial score and mark it as initial', () {
+        var actual = TennisCalculator.build(
+          playerAScore: Score.love,
+          playerBScore: Score.love,
+        );
 
-      expect(actual, const InitialTennisGame(
-          playerAScore: Score.Fifteen,
-          playerBScore: Score.Thirty,
-      ));
-    });
+        expect(actual, const InitialTennisGame(
+          playerAScore: Score.love,
+          playerBScore: Score.love,
+        ));
+      });
 
-    test('equality', () {
-      expect(
-        const InitialTennisGame(playerAScore: Score.Love, playerBScore: Score.Love),
-        const InitialTennisGame(playerAScore: Score.Love, playerBScore: Score.Love)
-      );
+      test('should be able to init with playing start when non nil, but not deuced', () {
+        var actual = TennisCalculator.build(
+          playerAScore: Score.fifteen,
+          playerBScore: Score.love,
+        );
+
+        expect(actual, const PlayingTennisGame(
+          playerAScore: Score.fifteen,
+          playerBScore: Score.love,
+        ));
+      });
+
+      test('should be able to init with deuce when both player score is Forty and Forty', () {
+        var actual = TennisCalculator.build(
+          playerAScore: Score.forty,
+          playerBScore: Score.forty,
+        );
+
+        expect(actual, const DeuceTennisGame());
+      });
+
+      test('should be able to init with player A advantage', () {
+        var actual = TennisCalculator.buildPlayerAAdvantage();
+
+        expect(actual, const PlayerAAdvantageTennisGame());
+      });
+
+      test('should be able to init with player B advantage', () {
+        var actual = TennisCalculator.buildPlayerBAdvantage();
+
+        expect(actual, const PlayerBAdvantageTennisGame());
+      });
+
+      test('should be able to init with player A Won', () {
+        var actual = TennisCalculator.buildPlayerAWon();
+
+        expect(actual, const PlayerAWonTennisGame());
+      });
+
+      test('should be able to init with player B Won', () {
+        var actual = TennisCalculator.buildPlayerBWon();
+
+        expect(actual, const PlayerBWonTennisGame());
+      });
     });
   });
 }
