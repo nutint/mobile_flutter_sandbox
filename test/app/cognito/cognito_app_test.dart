3910c37c2c9524f21b166f60aaa2fc23287da44f
Nat-macbook <nattha.int@gmail.com>
2023-01-29 15:38:05 +0700

    Add mockito and it's setup

--- test/app/cognito/cognito_app_test.dart
+++ test/app/cognito/cognito_app_test.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:my_app/apps/cognito/cognito_app.dart';
 import 'package:my_app/drivers/amplify/amplify_helper.dart';
 
-class MockedAmplifyHelper extends IAmplifyHelper {
-  const MockedAmplifyHelper();
+import 'cognito_app_test.mocks.dart';
 
-  @override
-  Future<void> configureAmplify() async {}
-}
+@GenerateMocks([IAmplifyHelper])
 
 void main() {
   testWidgets('should have login button', (widgetTester) async {
-    const mockedAmplifyHelper = MockedAmplifyHelper();
-    await widgetTester.pumpWidget(const CognitoApp(amplifyHelper: mockedAmplifyHelper));
+    var mockedAmplifyHelper = MockIAmplifyHelper();
+    await widgetTester.pumpWidget(CognitoApp(amplifyHelper: mockedAmplifyHelper));
     
     expect(find.byIcon(Icons.person), findsOneWidget);
   });
