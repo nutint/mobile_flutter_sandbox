ee6a768c6b99f183c800e679dda0014dee97ca0f
Nat-macbook <nattha.int@gmail.com>
2023-01-29 13:32:04 +0700

    Make cognito app stateful

--- lib/apps/cognito/cognito_app.dart
+++ lib/apps/cognito/cognito_app.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/apps/cognito/pages/login_page.dart';
 
-class CognitoApp extends StatelessWidget {
+class CognitoApp extends StatefulWidget {
   const CognitoApp({super.key});
+
+  @override
+  State<CognitoApp> createState() => _CognitoAppState();
+}
+
+class _CognitoAppState extends State<CognitoApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Cognito App",
       theme: ThemeData(
-        primarySwatch: Colors.brown
+          primarySwatch: Colors.brown
       ),
       home: const LoginPage(),
     );
